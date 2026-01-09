5600e0eae67a4ac0e7d545c1a8b404640db4413b
Baran İpek <93608825+Baranipk@users.noreply.github.com>
2026-01-09 21:29:14 +0300

    Update Door.cs

--- Assets/!Core/InteractableObjects/Door/Door.cs
+++ Assets/!Core/InteractableObjects/Door/Door.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
+    {
+        
+    }
+
+    public async void open()
     {
         
     }
